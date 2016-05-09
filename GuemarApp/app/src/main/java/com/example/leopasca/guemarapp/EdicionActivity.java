2097fd772f69dfe36ccf234b186a1df730a5a65e
leopasca <leopasca@hotmail.com>
2016-05-08 23:04:22 -0300

    Pro Final

--- GuemarApp/app/src/main/java/com/example/leopasca/guemarapp/EdicionActivity.java
+++ GuemarApp/app/src/main/java/com/example/leopasca/guemarapp/EdicionActivity.java
@@ -1,47 +1,45 @@
 package com.example.leopasca.guemarapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
+import com.joanzapata.pdfview.PDFView;
 
 import java.io.File;
+import java.io.StreamCorruptedException;
 
-public class EdicionActivity extends AppCompatActivity {
-
-    public final String GlobalUrl = "https://drive.google.com/file/d/0B0i3x2mQEke_b015ZXJQUXJjY0U/view";
+public class EdicionActivity extends Activity {
+    WebView webview;
+    PDFView pdfView;
+    public static final String FILE ="beitza.pdf";
     public void ObtenerReferencias ()
     {
-
+        webview = (WebView) findViewById(R.id.wbvPDF);
+        pdfView =(PDFView) findViewById(R.id.pdfview);
 
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion);
-        WebView webview = (WebView) this.findViewById(R.id.wbvPDF);
-        webview.getSettings().setJavaScriptEnabled(true);
-        loadWebViewLoad(webview);
+        ObtenerReferencias();
+        pdfView.fromAsset("beitza.pdf").load();
+
     }
 
-    private void loadWebViewLoad(WebView webview) {
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webview.getSettings().setSupportMultipleWindows(true);
-        webview.setWebViewClient(new WebViewClient());
-        webview.setWebChromeClient(new WebChromeClient());
-        webview.loadUrl(GlobalUrl);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
